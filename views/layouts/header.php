8a75d07581374e5a0a475fe2e5d75385ca17af73
henry-jacq <henryeditz267@gmail.com>
2024-01-04 00:46:35 +0530

    feat: JS files and header updated

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-md border-bottom py-1 fixed-top bg-body-tertiary mb-3 shadow" aria-label="header">
     <div class="container py-1">
-        <a class="navbar-brand fs-4 link-body-emphasis display-6 me-auto" href="/">
+        <a class="navbar-brand fs-4 fw-light link-body-emphasis me-auto" href="/">
             <img src="/assets/brand/photogram-logo.png" alt="logo" width="30" class="d-inline-block align-text-top">
             <div class="d-none d-sm-inline-block">
-                <label>Photogram</label>
+                Photogram
             </div>
         </a>
         <button class="navbar-toggler collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#slHeader" aria-controls="slHeader" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,8 +12,8 @@
         <div class="navbar-collapse collapse" id="slHeader">
             <div class="d-flex ms-auto">
                 <ul class="nav flex-nowrap align-items-center list-unstyled">
-                    <li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Write a blog">
-                        <a href="/" class="btn border border-secondary rounded-pill" type="button"><i class="bi bi-plus-circle me-2"></i>Post</a>
+                    <li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Create post">
+                        <a href="/" class="btn border border-secondary rounded-pill" type="button"><i class="bi bi-plus-square-dotted"></i></a>
                     </li>
                     <li class="nav-item ms-2" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Switch theme">
                         <a id="themeSwitcher" class="btn border border-secondary rounded-pill" role="button"><i class="bi bi-moon-stars"></i></a>
@@ -32,22 +32,29 @@
                             <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                 <img src="https://github.com/mdo.png" alt="mdo" width="38" height="38" class="border border-secondary rounded-circle">
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-end mt-2 shadow">
-                                <li>
-                                    <a class="dropdown-item" href="/profile" role="button"><i class="bi bi-person-circle me-2"></i>My Profile</a>
+                            <ul class="dropdown-menu dropdown-animation dropdown-menu-end pt-2 small mt-2" aria-labelledby="profileDropdown" data-bs-popper="static">
+                                <li class="px-2">
+                                    <div class="d-flex align-items-center position-relative btn bg-body-tertiary">
+                                        <div class="avatar avatar-story me-2">
+                                            <a href="#" class="d-block link-dark text-decoration-none" aria-expanded="false">
+                                                <img class="user-profile-img border rounded-circle" src="https://api.dicebear.com/6.x/shapes/svg?seed=1" width="36" height="36"></a>
+                                        </div>
+                                        <div>
+                                            <a class="h6 stretched-link text-decoration-none" href="/profile/henry">Henry</a>
+                                        </div>
+                                    </div>
+                                    <hr class="mt-2 mb-1">
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="/profile/edit" role="button"><i class="bi bi-pencil-square me-2"></i>Edit Profile</a>
+                                    <a class="dropdown-item" href="/edit-profile">
+                                        <i class="fa-fw bi bi-pencil me-2"></i>Edit profile
+                                    </a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" onclick="dialog('Not Implemented!',' This feature is not implemented');" role="button"><i class="bi bi-gear me-2"></i>Settings</a>
+                                    <a class="dropdown-item" href="/settings"><i class="bi bi-gear fa-fw me-2"></i>Settings</a>
                                 </li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="/logout"><i class="bi bi-box-arrow-left me-2"></i>Sign out</a>
-                                </li>
+                                <li class="dropdown-divider"></li>
+                                <li><a class="dropdown-item bg-danger-soft-hover" href="/logout"><i class="bi bi-box-arrow-left fa-fw me-2"></i>Sign Out</a></li>
                             </ul>
                         </div>
                     </li>
