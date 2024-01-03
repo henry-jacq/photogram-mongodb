3cf653a8edb061ea0895622f371a6f01c58e3bf0
henry-jacq <henryeditz267@gmail.com>
2024-01-04 00:25:15 +0530

    feat: SEO details updated

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,6 +1,27 @@
 <head>
-    <meta charset="utf-8">
+    <meta data-n-head="1" charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $this->title ?></title>
+    <link rel="icon" type="image/x-icon" href="/assets/brand/photogram-icon.png">
+    <meta name="author" content="Henry">
+    <meta property="og:image" content="/assets/brand/photogram-icon.png">
+    <meta property="site_name" content="Photogram">
+    <meta property="og:title" content="Photogram · Gallery of Memories">
+    <meta property="og:site_name" content="DOMAIN_NAME">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="/assets/brand/photogram-icon.png">
+    <meta property="og:image:alt" content="Photogram • Created by Henry">
+    <meta property="description" content="Create an account or log in to Photogram. Share photos &amp; videos with friends, family and other people you know.">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Photogram">
+    <meta name="twitter:description" content="Photogram is an easy-to-use web app for sharing photos with a clean and uncluttered interface.">
+    <meta name="twitter:image" content="/assets/screenshot-2.png">
+    
+    <!-- Favicon for photogram -->
+    <link rel="shortcut icon" href="/assets/brand/favicon.ico">
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
