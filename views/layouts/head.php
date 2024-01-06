2bbbe2cb07d8bdac20a7f4f4b118a312d95a23f4
henry-jacq <henryeditz267@gmail.com>
2024-01-07 00:05:36 +0530

    change: head and script layouts updated

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -17,11 +17,8 @@
     <meta name="twitter:title" content="Photogram">
     <meta name="twitter:description" content="Photogram is an easy-to-use web app for sharing photos with a clean and uncluttered interface.">
     <meta name="twitter:image" content="/assets/screenshot-2.png">
-    
-    <!-- Favicon for photogram -->
     <link rel="shortcut icon" href="/assets/brand/favicon.ico">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!-- Bootstrap Icons -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="/css/app.min.css" rel="stylesheet">
 </head>
--- views/layouts/script.php
+++ views/layouts/script.php
@@ -1,17 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-
-<!-- Theme Switcher -->
-<script src="/js/theme-switcher.js"></script>
-
-<!-- Welcome script -->
-<script src="/js/hello.js"></script>
-
-<!-- Jquery -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-
-<!-- Dialog JS -->
-<script src="/js/dialog/dialog.js"></script>
-
-<!-- Toast JS -->
-<script src="/js/toast/toast.js"></script>
+<script src="/js/vendor/popper.min.js"></script>
+<script src="/js/vendor/bootstrap.min.js"></script>
+<script src="/js/vendor/jquery.min.js"></script>
+<script src="/js/core.min.js"></script>
+<script src="/js/app.min.js"></script>
