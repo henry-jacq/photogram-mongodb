d629d43df260af3a66021930a32dd1196bedbd81
henry-jacq <henryeditz267@gmail.com>
2024-01-08 01:09:14 +0530

    feat: New updated header
    - Improved header UI
    - Improved responsiveness
    - Added color toggle button in dropdown

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,65 +1,71 @@
-<nav class="navbar navbar-expand-md border-bottom py-1 fixed-top bg-body-tertiary mb-3 shadow" aria-label="header">
-    <div class="container py-1">
-        <a class="navbar-brand fs-4 fw-light link-body-emphasis me-auto" href="/">
-            <img src="/assets/brand/photogram-logo.png" alt="logo" width="30" class="d-inline-block align-text-top">
-            <div class="d-none d-sm-inline-block">
-                Photogram
-            </div>
-        </a>
-        <button class="navbar-toggler collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#slHeader" aria-controls="slHeader" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon fs-6"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="slHeader">
-            <div class="d-flex ms-auto">
-                <ul class="nav flex-nowrap align-items-center list-unstyled">
-                    <li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Create post">
-                        <a href="/" class="btn border border-secondary rounded-pill" type="button"><i class="bi bi-plus-square-dotted"></i></a>
-                    </li>
-                    <li class="nav-item ms-2" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Switch theme">
-                        <a id="themeSwitcher" class="btn border border-secondary rounded-pill" role="button"><i class="bi bi-moon-stars"></i></a>
-                    </li>
-                    <li class="nav-item ms-2" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Notifications">
-                        <a class="btn border border-secondary rounded-pill position-relative" role="button">
-                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger px-1">
-                                9+
-                                <span class="visually-hidden">unread messages</span>
-                            </span>
-                            <i class="bi bi-bell"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item ms-2">
-                        <div class="dropdown text-end">
-                            <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://github.com/mdo.png" alt="mdo" width="38" height="38" class="border border-secondary rounded-circle">
-                            </a>
-                            <ul class="dropdown-menu dropdown-animation dropdown-menu-end pt-2 small mt-2" aria-labelledby="profileDropdown" data-bs-popper="static">
-                                <li class="px-2">
-                                    <div class="d-flex align-items-center position-relative btn bg-body-tertiary">
-                                        <div class="avatar avatar-story me-2">
-                                            <a href="#" class="d-block link-dark text-decoration-none" aria-expanded="false">
-                                                <img class="user-profile-img border rounded-circle" src="https://api.dicebear.com/6.x/shapes/svg?seed=1" width="36" height="36"></a>
-                                        </div>
-                                        <div>
-                                            <a class="h6 stretched-link text-decoration-none" href="/profile/henry">Henry</a>
-                                        </div>
+<div class="container mt-3">
+    <nav class="navbar navbar-expand-md bg-body-tertiary border rounded" aria-label="Photogram Navbar">
+        <div class="container-fluid gap-2">
+            <a class="navbar-brand fw-light fs-4 me-auto link-body-emphasis" href="/">
+                <img src="/assets/brand/photogram-logo.png" width="30" class="d-inline-block align-text-top">
+                <div class="d-none d-md-inline-block">
+                    <span>Photogram</span>
+                </div>
+            </a>
+            <button class="navbar-toggler border-0 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#photogramNavbar" aria-controls="photogramNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse d-md-flex collapse" id="photogramNavbar">
+                <div class="d-md-flex justify-content-md-end ms-auto">
+                    <ul class="navbar-nav gap-md-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/"><i class="bi bi-house-door fs-5" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Home"></i><span class="d-md-none ms-2">Home</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/discover"><i class="bi bi-compass fs-5" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Discover"></i><span class="d-md-none ms-2">Discover</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a role="button" class="nav-link active" aria-current="page"><i class="bi bi-plus-square fs-5" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Create Post"></i><span class="d-md-none ms-2">Create</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/activities"><i class="bi bi-heart fs-5" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="My Activities"></i><span class="d-md-none ms-2">Likes</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a role="button" class="nav-link active" aria-current="page">
+                                <div class="d-flex">
+                                    <div class="position-relative">
+                                        <i class="bi bi-bell fs-5"></i>
+                                        <span class="position-absolute top-0 start-75 translate-middle badge rounded-pill bg-danger fw-light fst-normal">3</span>
                                     </div>
-                                    <hr class="mt-2 mb-1">
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="/edit-profile">
-                                        <i class="fa-fw bi bi-pencil me-2"></i>Edit profile
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="/settings"><i class="bi bi-gear fa-fw me-2"></i>Settings</a>
-                                </li>
+                                    <span class="d-md-none ms-2 mt-1">Notifications</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link active" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                                <img class="img-fluid border rounded-circle" src="https://api.dicebear.com/6.x/shapes/svg?seed=1" width="26" height="26">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+                                </svg>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-md-end">
+                                <li><a class="dropdown-item" href="/profile/henry"><i class="bi bi-person-circle me-2"></i>My Profile</a></li>
+                                <li><a class="dropdown-item" href="/profile/edit"><i class="bi bi-pencil me-2"></i>Edit Profile</a></li>
+                                <li><a class="dropdown-item" href="/settings/account"><i class="bi bi-gear me-2"></i>Settings</a></li>
                                 <li class="dropdown-divider"></li>
-                                <li><a class="dropdown-item bg-danger-soft-hover" href="/logout"><i class="bi bi-box-arrow-left fa-fw me-2"></i>Sign Out</a></li>
+                                <li><a class="dropdown-item" href="/logout"><i class="bi bi-box-arrow-left me-2"></i>Sign Out</a></li>
+                                <li class="dropdown-divider"></li>
+                                <li>
+                                    <div class="d-flex justify-content-center gap-2 align-items-center px-2 py-0">
+                                        <span class="user-select-none">Mode:</span>
+                                        <button type="button" class="btn border" data-bs-theme-value="light">
+                                            <i class="bi bi-sun fs-5"></i>
+                                        </button>
+                                        <button type="button" class="btn border" data-bs-theme-value="dark">
+                                            <i class="bi bi-moon-stars fs-5"></i>
+                                        </button>
+                                    </div>
+                                </li>
                             </ul>
-                        </div>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
