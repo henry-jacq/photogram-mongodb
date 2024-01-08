4938eb3dc06e9a6135084e2525fdcce71602f013
henry-jacq <henryeditz267@gmail.com>
2024-01-08 12:34:32 +0530

    feat: Feed view toggle button

--- views/templates/user/home.php
+++ views/templates/user/home.php
@@ -1,10 +1,20 @@
 <div class="container">
     <div class="stories-section py-4">
-        <h2 class="fs-4 lead">Stories</h2>
+        <h3 class="fw-light mt-3">Stories</h3>
         <hr class="m-0">
     </div>
     <div class="post-feed-section py-4">
-        <h2 class="fs-4 lead">Feed</h2>
+        <div class="d-flex justify-content-between align-items-center">
+            <h3 class="fw-light mt-3">My Feed</h3>
+            <div class="d-none d-md-inline-block">
+                <div class="btn-group btn-group-sm" role="group" aria-label="Basic radio toggle button group">
+                    <input type="radio" class="btn-check" name="view_mode" id="btnRadioGrid" autocomplete="off" checked="">
+                    <label class="btn btn-outline-prime rounded-start-4" for="btnRadioGrid"><i class="bi bi-grid-3x3 me-2"></i>Grid</label>
+                    <input type="radio" class="btn-check" name="view_mode" id="btnRadioList" autocomplete="off">
+                    <label class="btn btn-outline-prime rounded-end-4" for="btnRadioList"><i class="bi bi-grid-1x2 me-2"></i>List</label>
+                </div>
+            </div>
+        </div>
         <hr class="m-0">
     </div>
 </div>
