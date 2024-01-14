3f485d93ed04604babba94518538e61801e0e946
henry-jacq <henryeditz267@gmail.com>
2024-01-14 21:48:06 +0530

    feat: custom page not found view added
    - Custom error view page added
    - It shows default error page or custom page depending on config
    - If view fails to deliver, it fallback to JSON response

--- views/templates/error.php
+++ views/templates/error.php
@@ -1,12 +1,11 @@
-<div class="d-flex flex-wrap align-items-center justify-content-center min-vh-100">
-    <div class="container p-5">
+<div class="d-flex align-items-center justify-content-center min-vh-100">
+    <div class="p-md-5 py-4">
         <div class="row mx-auto text-center py-5 bg-body-tertiary rounded">
-            <span class="display-1 d-block"><?= $code?></span>
+            <span class="display-2 d-block"><?= $code ?></span>
             <span class="display-4 d-block mb-3">Error</span>
-            <!-- <div class="mb-4 lead">The page you are looking for was not found or other error occured.</div> -->
-            <div class="mb-4 lead text-truncate"><?= $exception?></div>
+            <div class="mb-4 lead px-sm-5">The page you are looking for was not found or other error occured.</div>
             <div class="d-grid col-sm-8 col-md-5 col-lg-3 mx-auto">
-                <a href="/" class="btn text-secondary-emphasis border-0 text-decoration-none hvr-icon-back"><i class="fa fa-arrow-left me-2 hvr-icon"></i>Back to Storylog</a>
+                <a href="/" class="btn text-body border-0 text-decoration-none hvr-icon-back"><i class="fa fa-arrow-left me-2 hvr-icon"></i>Back to Photogram</a>
             </div>
         </div>
     </div>
