a7cbca151dd588250f9637919c13e2e5d8478067
henry-jacq <henryeditz267@gmail.com>
2024-01-20 16:40:24 +0530

    change: dropzone css added

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -19,6 +19,7 @@
     <meta name="twitter:image" content="/assets/screenshot-2.png">
     <link rel="shortcut icon" href="/assets/brand/favicon.ico">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/dropzone.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="/css/app.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
