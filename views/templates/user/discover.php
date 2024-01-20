34a6fa126e3a9259f2389716c2f6d1ecf2f3f8f2
henry-jacq <henryeditz267@gmail.com>
2024-01-20 16:34:29 +0530

    change: discover page updated

--- views/templates/user/discover.php
+++ views/templates/user/discover.php
@@ -2,12 +2,11 @@
     <div class="d-flex align-items-center justify-content-center">
         <div class="col-12 col-md-7 col-lg-6 mt-3 mb-5">
             <form class="text-body position-relative ms-auto my-md-0 my-sm-3" role="search">
-                <div class="d-none d-md-inline-block">
-                    <i class="bi bi-search small text-body border-0 ps-3 position-absolute top-50 start-0 translate-middle-y"></i>
-                </div>
-                <input class="form-control shadow px-md-5" type="search" placeholder="Discover peoples ..." aria-label="Search" aria-describedby="search-blogs">
-                <div class="d-none d-md-inline-block">
-                    <button class="btn btn-comment-send focus-ring focus-ring-prime border-0 position-absolute top-50 end-0 translate-middle-y" type="button"><i class="bi bi-toggles2"></i></button>
+                <div class="input-group mb-3">
+                    <input class="form-control shadow pe-md-5" type="search" name="search" placeholder="Search" aria-label="Search" aria-describedby="search-blogs">
+                    <span class="input-group-text px-3" id="icon-click" role="button" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Search">
+                        <i class="bi-search" id="icon"></i>
+                    </span>
                 </div>
             </form>
         </div>
