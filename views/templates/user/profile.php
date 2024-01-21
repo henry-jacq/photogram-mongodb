32090d1720bef02d8bfdf749aeba39e4a9ab0581
henry-jacq <henryeditz267@gmail.com>
2024-01-21 14:30:32 +0530

    feat: display posts using components

--- views/templates/user/profile.php
+++ views/templates/user/profile.php
@@ -9,14 +9,19 @@
             <img class="img-fluid rounded-circle" src="https://api.dicebear.com/6.x/shapes/svg?seed=1" alt="">
         </div>
         <div class="position-absolute bottom-0 end-0 p-2">
-            <a href="/profile/edit" class="btn btn-prime btn-sm"><i class="bi bi-pencil me-1"></i>Edit Profile</a>
+            <?php if ($profileUser['username'] == $user['username']) : ?>
+                <a href="/profile/edit" class="btn btn-prime btn-sm"><i class="bi bi-pencil me-1"></i>Edit Profile</a>
+            <?php else : ?>
+                <button class="btn btn-sm btn-primary btn-follow" data-id="<?= $profileUser['_id'] ?>"><i class="bi-person-add me-1"></i>Follow</button>
+                <button class="btn btn-sm btn-secondary" onclick="dialog('Not Implemented!',' This feature is not implemented');"><i class="bi bi-chat-left-text-fill me-1"></i>Message</button>
+            <?php endif; ?>
         </div>
     </div>
     <div class="container mt-5">
         <div class="row mx-2 mb-2">
             <div class="col-md-7">
-                <h5 class="m-0"><?= ucfirst($user['fullname']) ?> </h5>
-                <p class="mb-2">@<?= $user['username'] ?> <span class="small mb-2"> • App Developer</span></p>
+                <h5 class="m-0"><?= ucfirst($profileUser['fullname']) ?> </h5>
+                <p class="mb-2">@<?= $profileUser['username'] ?> <span class="small mb-2"> • App Developer</span></p>
                 <p class="text-secondary small"><i class="bi bi-geo-alt me-1"></i>Chennai, India</p>
                 <p>#!/bin/bash<br>
                     Full stack developer</p>
@@ -45,9 +50,25 @@
                 </div>
             </div>
         </div>
-        <div class="ms-3 mt-4">
+        <div class="ms-2 mt-4">
             <h4 class="fw-normal">Posts</h4>
         </div>
         <hr class="mt-2 mb-3">
+        <div class="user-posts-section mb-5">
+            <?php
+            if ($posts !== false) {
+                $this->renderComponent('card', [
+                    'posts' => $posts,
+                    'user' => $user,
+                    'name' => $name,
+                    'profileUser' => $profileUser
+                ]);
+            } else if ($title !== 'Home') {?>
+                <div class="text-center py-5">
+                    <i class="bi bi-plus-circle display-4 mb-4"></i>
+                    <p class="text-muted text-center align-items-center mb-0 ">No posts yet!</p>
+                </div>
+            <?php } ?>
+        </div>
     </div>
 </div>
