75f9eb11f0a6cc259ae9034a036aadb822c65647
henry-jacq <henryeditz267@gmail.com>
2024-01-22 14:44:06 +0530

    change: display posts in profile

--- app/Controller/ProfileController.php
+++ app/Controller/ProfileController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use App\Core\View;
-use App\Core\Config;
+use App\Model\Post;
+use App\Model\User;
 use App\Core\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -11,20 +12,33 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 class ProfileController extends Controller
 {
     public function __construct(
-        private readonly View $view
+        private readonly View $view,
+        private readonly Post $post,
+        private readonly User $user
     ) {
         parent::__construct($view);
     }
 
     public function profile(Request $request, Response $response): Response
     {
-        $name = $request->getAttribute('name');
-        $args = [
-            'name' => $name,
-            'title' => ucfirst($name) . "'s Profile",
-            'user' => $request->getAttribute('userData')
-        ];
-        return $this->render($response, 'user/profile', $args);
+        $userData = $request->getAttribute('userData');
+        $name = strtolower($request->getAttribute('name'));
+        $profile = $this->user->exists([
+            'username' => $name, 'email' => null
+        ]);
+
+        if ($profile) {
+            $args = [
+                'name' => $name,
+                'user' => $userData,
+                'profileUser' => $profile,
+                'title' => ucfirst($name) . "'s Profile",
+                'posts' => $this->post->getPostsById($profile['_id'])
+            ];
+            return $this->render($response, 'user/profile', $args);
+        }
+        
+        return $this->renderErrorPage($response);
     }
 
     public function edit(Request $request, Response $response): Response
