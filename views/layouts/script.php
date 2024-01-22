7988483a7855915fcc1c193facab9837cb35fa2a
henry-jacq <henryeditz267@gmail.com>
2024-01-22 14:15:21 +0530

    change: script updated
    - Added new dependencies

--- views/layouts/script.php
+++ views/layouts/script.php
@@ -1,5 +1,12 @@
 <script src="/js/vendor/popper.min.js"></script>
 <script src="/js/vendor/bootstrap.min.js"></script>
 <script src="/js/vendor/jquery.min.js"></script>
-<script src="/js/core.min.js"></script>
+<?php if (!in_array($this->title, ['Login', 'Register', 'Forgot password', 'Share Memories'])) : ?>
+    <script src="/js/core.min.js"></script>
+<?php endif; ?>
+<script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
+<script src="https://unpkg.com/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"></script>
+<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+<script src="https://kit.fontawesome.com/cd2caad5e8.js" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="/js/app.min.js"></script>
