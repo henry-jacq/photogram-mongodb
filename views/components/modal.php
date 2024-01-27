595809ca301863b06ed2fd466296e0b2f217cf61
henry-jacq <henryeditz267@gmail.com>
2024-01-27 14:10:00 +0530

    feat: Modal zoom in out animation

--- views/components/modal.php
+++ views/components/modal.php
@@ -1,7 +1,7 @@
 <div id="modalsGarbage">
-    <div class="modal fade user-select-none" data-bs-backdrop="static" id="dummy-dialog-modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+    <div class="modal fade zoom-fade user-select-none" data-bs-backdrop="static" id="dummy-dialog-modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content blur">
+            <div class="modal-content blur border-0" style="box-shadow: rgba(111, 66, 193, 0.3) 0px 0px 0px 3px">
                 <div class="modal-header">
                     <h4 class="modal-title"></h4>
                 </div>
