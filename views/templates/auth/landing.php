53e70a4b28f2e18fa8cc0eabf9d4931ce81f85c4
henry-jacq <henryeditz267@gmail.com>
2024-01-29 14:22:53 +0530

    feat: new landing page with pro

--- views/templates/auth/landing.php
+++ views/templates/auth/landing.php
@@ -1,17 +1,252 @@
-<div class="container p-3">
-    <div class="row flex-lg-row-reverse align-items-center g-4 border rounded-5 p-3 mt-3 mb-4 shadow-lg gap-5">
+<div class="container p-3 d-flex flex-column gap-3 mb-5">
+    <div class="row flex-lg-row-reverse align-items-center g-4 border rounded-5 p-3 mt-3 mb-4 shadow-lg gap-5 py-5">
         <div class="col-10 col-sm-8 col-lg-6 d-none d-lg-inline-block">
             <img src="/assets/images/camera.svg" class="d-block mx-lg-auto img-fluid" width="700" height="500" loading="lazy">
         </div>
         <div class="col-lg-5">
-            <img src="/assets/brand/photogram-logo.png" class="d-block mx-lg-start img-fluid" width="70" height="70" loading="lazy">
-            <h1 class="display-6 text-body mt-3">Photogram</h1>
-            <p class="lead mb-3">Welcome to photogram!</p>
-            <p class="lead">An ultimate photo-sharing platform! With Photogram, you can showcase your favourite moments, experiences, and visual creations.
+            <img src="/assets/brand/photogram-logo.png" class="d-block mx-lg-start img-fluid" width="90" height="90" loading="lazy">
+            <h1 class="display-5 text-body mt-3">Photogram</h1>
+            <p class="fs-5 mb-3">Sharing Memories!</p>
+            <p class="fs-5">An Ultimate Photo-Sharing Platform! With Photogram, you can showcase your favourite moments, experiences, and visual creations.
             </p>
-            <p class="lead">Our platform offers a simple and user-friendly interface, allowing you to effortlessly share your visual creations with the world. Whether you're the one who loves to capture and share moments with others, this is the perfect platform for you.</p>
-            <p class="lead">Every picture tells a unique story.</p>
-            <a href="/login" class="btn btn-outline-prime btn-lg mt-3 mb-4 me-md-2">Get started !</a>
+            <p class="fs-5">Our platform offers a simple and user-friendly interface, allowing you to effortlessly share your visual creations with the world. Whether you're the one who loves to capture and share moments with others, this is the perfect platform for you.</p>
+            <p class="fs-5">Every picture tells a unique story.</p>
+            <a href="/login" class="btn btn-outline-prime btn-lg mt-3 mb-4 me-md-2">Get started!</a>
+        </div>
+    </div>
+    <div class="row p-5 mt-3 mb-4 border rounded-5 shadow-lg gap-5">
+        <div class="container-fluid py-4">
+            <h1 class="display-5 mb-3 col-md-12 col-lg-8">Unlock Limitless Possibilities With Photogram Pro</h1>
+            <p class="fs-4 mb-5">Elevate your Photogram Experience! 🚀</p>
+            <p class="fs-4">✨ Exclusive Personalization</p>
+            <p class="fs-5 mb-4 ms-4">Customize your profile with background cover images and unique themes, making it truly yours.</p>
+            <p class="fs-4">🎨 Unleash Your Creativity</p>
+            <p class="fs-5 mb-4 ms-4">Share your moments in a richer narrative with the ability to create posts with multiple images.</p>
+            <p class="fs-4">📅 Time Management</p>
+            <div class="fs-5 mb-4 ms-4">Plan your content strategy ahead with the ability to schedule posts for future publishing.</div>
+            <p class="fs-4">📊 Analytics Insights</p>
+            <p class="fs-5 mb-4 ms-4">Dive into detailed analytics to track the performance of your posts and optimize your content strategy.</p>
+            <p class="fs-4">🚫 Ad-Free Zone</p>
+            <p class="fs-5 mb-4 ms-4">Immerse yourself in an ad-free environment for a smoother and distraction-free experience.</p>
+            <!-- <p class="fs-4 px-1 mb-5">Ready to unlock the full potential of Photogram?<br> Upgrade to Photogram Pro now and let your creativity shine! 🌈✨</p> -->
+            <!-- <button class="btn btn-outline-warning btn-lg" type="button"><i class="bi bi-star me-2"></i>Unlock Photogram Pro</button> -->
+        </div>
+    </div>
+    <div class="row border rounded-5 p-3 mt-3 mb-4 shadow-lg gap-5">
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 text-center mt-4 mb-3">Pricing</h1>
+            <p class="fs-5 text-body-secondary">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+        </div>
+        <main>
+            <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+                <div class="col">
+                    <div class="card mb-4 rounded-3 shadow-sm">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Free</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">₹0<small class="text-body-secondary fw-light">/month</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>Download Posts as Zip</li>
+                                <li>Light/Dark Theme</li>
+                                <li>Sharing Post Link</li>
+                                <li>Deleting Posts</li>
+                                <li>Turn On/Off Comments</li>
+                                <li>Editing Post Text</li>
+                                <li>Post Image Full Preview</li>
+                            </ul>
+                            <a href="/register" type="button" class="w-100 btn btn-lg btn-outline-prime">Get started for free</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card mb-4 rounded-3 shadow-sm border-prime">
+                        <div class="card-header py-3 text-bg-prime border-prime">
+                            <h4 class="my-0 fw-normal">Pro</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">₹70<small class="text-body-secondary fw-light">/month</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>Multiple Image Posts</li>
+                                <li>Posts Analytics</li>
+                                <li>Ad-Free Experience</li>
+                                <li>Scheduled Posts</li>
+                                <li>Enhanced Post Editing</li>
+                                <li>Caption Generator</li>
+                                <li>Profile Cover Image</li>
+                            </ul>
+                            <button type="button" class="w-100 btn btn-lg btn-prime">Get started with Pro</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card mb-4 rounded-3 shadow-sm border-prime">
+                        <div class="card-header py-3 text-bg-prime border-prime">
+                            <h4 class="my-0 fw-normal">Premier</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">₹699<small class="text-body-secondary fw-light">/yearly </small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>Multiple Image Posts</li>
+                                <li>Posts Analytics</li>
+                                <li>Ad-Free Experience</li>
+                                <li>Scheduled Posts</li>
+                                <li>Enhanced Post Editing</li>
+                                <li>Caption Generator</li>
+                                <li>Profile Cover Image</li>
+                            </ul>
+                            <button type="button" class="w-100 btn btn-lg btn-prime">Get started with Premier</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <div class="row border rounded-5 p-3 mt-3 mb-4 shadow-lg gap-5">
+        <h2 class="display-6 text-center mt-4 mb-3">Compare Plans</h2>
+
+        <div class="table-responsive px-sm-5">
+            <table class="table text-center">
+                <thead>
+                    <tr>
+                        <th style="width: 34%;"></th>
+                        <th class="fs-5 fw-semibold" style="width: 33%;">Free</th>
+                        <th class="fs-5 fw-semibold" style="width: 33%;">Pro</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Theme Switching</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Multiple Posts</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Download Posts</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Scheduled Posts</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Posts Full Preview</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Posts Analytics</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Search Peoples</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Caption Generator</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Sharing Links</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-start fw-normal fs-5">Advanced Post Editing</th>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                        </td>
+                        <td>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
+                            </svg>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
