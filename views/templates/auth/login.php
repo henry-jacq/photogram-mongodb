19789057eaf82b0ec556af6c5f49a072e664d6a1
henry-jacq <henryeditz267@gmail.com>
2024-02-04 13:39:42 +0530

    new: login with google button added

--- views/templates/auth/login.php
+++ views/templates/auth/login.php
@@ -1,7 +1,7 @@
 <div class="h-100 d-flex align-items-center justify-content-center row user-select-none min-vh-100">
     <div class="py-3 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
         <form class="user-login-form" method="post" autocomplete="off">
-            <div class="form-control p-4 shadow-lg bg-opacity-25">
+            <div class="form-control px-4 py-3 bg-dark shadow-lg bg-opacity-25">
                 <img src="/assets/brand/photogram-logo.png" alt="logo" class="img-fluid mx-auto d-block mb-2" width="63" height="63">
                 <h4 class="fw-light text-center mb-4">Photogram</h4>
                 <hr class="mb-4">
@@ -18,7 +18,7 @@
                 <div class="row mb-3">
                     <div class="col text-start">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="rememberMe" role="button">
+                            <input class="form-check-input bg-transparent" type="checkbox" id="rememberMe" role="button">
                             <label class="form-check-label" for="rememberMe">Remember me</label>
                         </div>
                     </div>
@@ -26,11 +26,11 @@
                         <a href="/forgot-password">Forgot password?</a>
                     </div>
                 </div>
-
-                <div class="d-grid">
-                    <button type="submit" class="btn btn-prime btn-login mb-3">Login now!</button>
+                <div class="d-grid gap-3">
+                    <button type="submit" class="btn btn-login text-body border-0 bg-prime bg-opacity-75 focus-ring focus-ring-prime">Login now!</button>
+                    <button type="button" class="btn btn-google-login border border-body bg-body bg-opacity-25 focus-ring focus-ring-secondary"><i class="bi bi-google me-2"></i>Login With Google</button>
+                    <p class="text-center text-muted">Want to join Photogram? <a href="/register">Register now</a>.</p>
                 </div>
-                <p class="text-center text-muted">Want to join Photogram? <a href="/register">Register now</a>.</p>
             </div>
         </form>
     </div>
