84108903dd8c3be7a97d95832fbc0a21973812a8
henry-jacq <henryeditz267@gmail.com>
2024-02-05 12:17:41 +0530

    minor changes

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="/css/app.min.css" rel="stylesheet">
     <?php if (isset($auth_css)):?>
-    <link href="/css/entry.css" rel="stylesheet">
+    <link href="/css/auth.min.css" rel="stylesheet">
     <?php endif; ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
